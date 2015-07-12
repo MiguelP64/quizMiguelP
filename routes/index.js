ca377474a8239acf0f1aa5227552cb30b3eb6cdd
MiguelP64 <mischagrx@gmail.com>
2015-07-12 17:24:50 +0200

    Ej mod 6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var quiz_Controller = require('../controllers/quiz_controller');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
 });
+
+router.get('/quizes/question', quiz_Controller.question);
+router.get('/quizes/answer', quiz_Controller.answer);
+
+/* GET autores  */
+
+router.get('/author', function(req, res, next) {
+  res.render('author', { autor: 'Miguel P64' });
+});
+
 
 module.exports = router;
